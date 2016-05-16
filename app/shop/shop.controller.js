671569f26a128bf935f0f9b6e91149f65c22a708
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-05-16 11:33:56 -0700

    add category and name filters

--- app/shop/shop.controller.js
+++ app/shop/shop.controller.js
@@ -5,9 +5,20 @@
   .controller('Shop', function($scope, Shop, Checkout) {
     $scope.view = {};
     $scope.view.bag = Checkout.bag;
-
+    $scope.view.categories = [];
+    $scope.view.quantities = [1,2,3,4,5,6,7,8,9,10];
     Shop.getItems().then(function(data) {
       $scope.view.items = data;
+
+      for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+
+        for (var j = 0; j < item.categories.length; j++) {
+          if ($scope.view.categories.indexOf(item.categories[j]) === -1) {
+            $scope.view.categories.push(item.categories[j]);
+          }
+        }
+      }
     });
     $scope.view.addItem = function(item) {
       Checkout.addItem(item);
